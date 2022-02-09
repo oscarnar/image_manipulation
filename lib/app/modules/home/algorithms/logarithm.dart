35aeb47e0cc1ba27a3f684aaae029bfc96a222fc
oscarnar <orosas58@gmail.com>
2022-02-09 10:33:37 -0500

    Updated logarithmic function

--- lib/app/modules/home/algorithms/logarithm.dart
+++ lib/app/modules/home/algorithms/logarithm.dart
@@ -2,26 +2,25 @@ import 'dart:math';
 
 import 'package:image/image.dart';
 
-class Log {
-  Image imagen;
-  int c;
-  Log(this.imagen, this.c);
-}
-
-/// Top level function
 Future<Image> logarithmic(Log data) async {
-  int wi = data.imagen.width;
-  int he = data.imagen.height;
-  for (int x = 0; x < wi; x++) {
-    for (int y = 0; y < he; y++) {
-      int red = getRed(data.imagen[y * wi + x]);
-      int blue = getBlue(data.imagen[y * wi + x]);
-      int green = getGreen(data.imagen[y * wi + x]);
+  int width = data.image.width;
+  int height = data.image.height;
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      final red = getRed(data.image[y * width + x]);
+      final blue = getBlue(data.image[y * width + x]);
+      final green = getGreen(data.image[y * width + x]);
       int newRed = (data.c * (log(red) / ln10)).clamp(0, 255).toInt();
       int newBlue = (data.c * (log(blue) / ln10)).clamp(0, 255).toInt();
       int newGreen = (data.c * (log(green) / ln10)).clamp(0, 255).toInt();
-      data.imagen.setPixelRgba(x, y, newRed, newGreen, newBlue);
+      data.image.setPixelRgba(x, y, newRed, newGreen, newBlue);
     }
   }
-  return data.imagen;
+  return data.image;
+}
+
+class Log {
+  Image image;
+  int c;
+  Log(this.image, this.c);
 }
